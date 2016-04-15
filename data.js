336a990d4a0c7795bfd51781b3fa209ec210cf05
Kevin Nguyen <account@kevinwin.com>
2016-04-15 11:17:41 -0700

    Initial commit

---
+++ data.js
@@ -0,0 +1,7 @@
+var adjectives = ['Happy', 'Glorious', 'Amazing', 'Jocular', 'Beautiful', 'Stellar', 'Fantastic', 'Glowing', 'Weary', 'Propitious', 'Wonderful', 
+'Marvelous', 'Delightful', 'Lovely', 'Vivacious', 'Fabulous', 'Exuberant', 'Scrumptious', 'Cooperative', 'Asteroid Dodging', 'Salubrious',
+'Well-rested', 'Context-retaining', 'Socializing', 'Brobdignagian', 'Supine', 'Reactive', 'Dedicated', 'Excellent', 'Silly',
+'Coherent', 'Sanguine', 'Studious', 'Vibrant', 'Dilligent', 'Oxidizing', 'Magnificent', 'Smarty-pants', 'Tenacious',
+'Shiny', 'Hypnotic', 'Authentic', 'Prominent', 'Winsome', 'Creative', 'Accomplished', 'Scootin', 'Francophone', 'Mirthful',
+'Judicious', 'Aurelian', 'Jovial', 'Incredible', 'Ripened', 'Adventurous', 'Resilient', 'Jaunty', 'Wizened', 'Jurrasic',
+'Astute', 'Emerald', 'Brilliant', 'Solo-ing']
